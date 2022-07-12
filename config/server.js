5bf5e595a8ae7e8f1e244807168e5cfe0742bc5c
andrew.s.dterra <a.shirin@dterra.eu>
2022-07-12 11:13:42 +0300

    First commit

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['K1EUjKZUb5ER9tlO3m9EAQ==', 'Q2nVa93TTOvLwcnGxW5sJA==', 'EPacmeXIfYn73BuUdMBhJg==', 'dCKx/xJuVxc/HCEdeHBArA==']),
   },
 });
